2189e08f1058bea14052a1eab733590df9259080
D7703_06 <d7703_06@d7703-06ui-imac.local>
2018-06-21 18:46:36 +0900

    Initial Commit

---
+++ Text123/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Text123
+//
+//  Created by D7703_06 on 2018. 6. 21..
+//  Copyright © 2018년 IceArrow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
